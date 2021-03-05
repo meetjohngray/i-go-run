78a40ea0a2f2ba5e23bf2c15a19e0b0905ea9f17
jgray1 <john.edwin.gray@gmail.com>
2021-03-06 12:00:51 +1300

    import dayjs and begin implementation of date conversion

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react'
 import { useQuery } from 'react-query'
-import { Flex, Heading, Table, Thead, Tbody, Tr, Th, Td, Text } from '@chakra-ui/react'
-// import runData from '../data/runData.json'
+import { Flex, Heading, Table, Thead, Tbody, Tr, Th, Td, Link, Text } from '@chakra-ui/react'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import dayjs from 'dayjs'
+import data from '../data/runData.json'
 
-const fetchRuns = async () => {
-  const response = await fetch('https://api.sheety.co/95b80a7b10ebccf74864840085f03df1/iGoRun/sheet1')
-  const data = await response.json()
-  return data
-}
+dayjs.extend(customParseFormat)
+
+// const fetchRuns = async () => {
+//   const response = await fetch('https://api.sheety.co/95b80a7b10ebccf74864840085f03df1/iGoRun/sheet1')
+//   const data = await response.json()
+//   return data
+// }
 
 function Summary () {
   function getMiles (distance) {
     return (distance * 0.0006213712).toFixed(2)
   }
-  const [runs, setRuns] = useState({})
+  console.log(data)
+  // const [runs, setRuns] = useState({})
 
-  const { isLoading, error, data } = useQuery('runData', fetchRuns, { refetchOnWindowFocus: false })
+  // const { isLoading, error, data } = useQuery('runData', fetchRuns, { refetchOnWindowFocus: false })
 
-  if (isLoading) return <span>Loading...</span>
-  if (error) return <span>Ooops! Something went wrong.</span>
+  // if (isLoading) return <span>Loading...</span>
+  // if (error) return <span>Ooops! Something went wrong.</span>
   return (
     <>
       <Heading as='h2'>Your Runs</Heading>
@@ -36,10 +41,10 @@ function Summary () {
             {data.sheet1.map((item) => {
               return (
                 <Tr key={item.id}>
-                  <Td>{item.dateTime}</Td>
+                  <Td>{dayjs(item.dateTime, ['MMMM-DD-YYYY', 'MMMM-D-YYYY']).format('YYYY-MM-DD [at] H:mm:ss')}</Td>
                   <Td>{getMiles(item.distance)} Miles</Td>
-                  <Td>{item.elapsedTime}</Td>
-                  <Td><a href={item.stravaLink}>Strava</a></Td>
+                  <Td>{dayjs(item.elapsedTime, ['HH:mm:ss', 'mm:ss']).format('H:mm:ss')}</Td>
+                  <Td><Link href={item.stravaLink}>Strava</Link></Td>
                 </Tr>)
             })
             }
