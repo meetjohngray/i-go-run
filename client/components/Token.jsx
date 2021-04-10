3f416b87a9a44aa4d53fc1c61489b855113ee33b
John Gray <john.edwin.gray@gmail.com>
2021-04-10 18:16:23 +1200

    Token.jsx checks for auth and redirects

--- client/components/Token.jsx
+++ client/components/Token.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import queryString from 'query-string'
+import { push } from 'connected-react-router'
+import { useDispatch, useSelector } from 'react-redux'
+import { validateStravaToken } from '../redux/actions/auth'
 
 const Token = () => {
-  return (
-    <div>Token received</div>
-  )
+  const location = useSelector((state) => state.router.location)
+  // Get the current auth state and setup a dispatch
+  const auth = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // Check if user/app is authenticated
+    const { code } = queryString.parse(location.search)
+    if (!auth.isAuthenticated && code) {
+    // Get code from the query string
+      // const { code } = queryString.parse(location.search)
+      // Dispatch the validation code to be handled by saga
+      dispatch(validateStravaToken(code))
+    } else {
+      // Push back to the home page bc the user is authenticated or there is no code
+      dispatch(push('/'))
+    }
+  }, [])
+
+  return null
 }
 
 export default Token
