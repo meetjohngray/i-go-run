30ea4d0f2edfcf85566f35a27d8df4a67554e3b7
John Gray <john.edwin.gray@gmail.com>
2021-04-28 19:18:44 +1200

    save state to local storage and subscribe store to it

--- client/configureStore.js
+++ client/configureStore.js
@@ -2,14 +2,19 @@ import { createBrowserHistory } from 'history'
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import createSagaMiddleware from 'redux-saga'
+import throttle from 'lodash/throttle'
+import { loadState, saveState } from './localStorage'
 
 import createRootReducer from './redux/reducers'
 import rootSaga from './redux/sagas'
+import { last } from 'lodash'
 
 const sagaMiddleware = createSagaMiddleware()
 export const history = createBrowserHistory()
 
-const configureStore = (initialState = {}) => {
+const persistedState = loadState() //Get the state from local storage
+
+const configureStore = () => {
   const enhancers = []
   const __DEV__ = process.env.NODE_ENV !== 'production'
   if (__DEV__) {
@@ -22,9 +27,22 @@ const configureStore = (initialState = {}) => {
   const middleware = [routerMiddleware(history), sagaMiddleware]
   const rootReducer = createRootReducer(history)
 
-  const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), ...enhancers))
+  // Use persisted state so on boot the app will use the data that exists in the state
+  const store = createStore(rootReducer, persistedState, compose(applyMiddleware(...middleware), ...enhancers))
 
   sagaMiddleware.run(rootSaga)
+
+  // Subscribe to store updates so we can action saves on change to state
+  store.subscribe(
+    // Throttle the save
+    throttle(() => {
+      saveState({
+        auth: store.getState().auth // Only save these sections of the auth
+        // if we want to save other state we can add it in here
+        // activities: store.getState().activities, etc!
+      })
+    }, 1000) // 1 per second max!
+  )
 
   return store
 }
